b8de20540e50cf171a71fe7c91e321f20fa6e774
Jakob Øvrum <jovrum@cisco.com>
2018-06-01 15:33:56 +0200

    Rubocop fix

--- bin/check-puppet-disabled.rb
+++ bin/check-puppet-disabled.rb
@@ -25,10 +25,10 @@ require 'json'
 
 class CheckPuppetDisabled < Sensu::Plugin::Check::CLI
   option :lockfile,
-    short: '-l PATH',
-    long: '--lockfile PATH',
-    description: 'path to puppet agent disable lockfile',
-    default: '/etc/puppetlabs/puppet/agent_disabled.lock'
+         short: '-l PATH',
+         long: '--lockfile PATH',
+         description: 'path to puppet agent disable lockfile',
+         default: '/etc/puppetlabs/puppet/agent_disabled.lock'
 
   def run
     lockfile_path = config[:lockfile]
